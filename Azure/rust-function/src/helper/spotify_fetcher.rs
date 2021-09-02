a82db2950521a06a7bf2e044cd5325f21543411a
AndersNS <anders.slinde@gmail.com>
2021-09-02 08:19:06 +0200

    Tried to fix some stuff

--- Azure/rust-function/src/helper/spotify_fetcher.rs
+++ Azure/rust-function/src/helper/spotify_fetcher.rs
@@ -1,17 +1,18 @@
 use crate::helper::link_parser::LinkType;
-use std::error::Error;
-use aspotify::{Client, ClientCredentials};
+use aspotify::{Client, ClientCredentials, Error, Page, Response, TrackSimplified};
 
-
-async fn get_metadata(link_type: LinkType) -> Result<(), Error> {
-    let credentials = ClientCredentials::from_env()
-        .expect("CLIENT_ID and CLIENT_SECRET not found.");
+async fn get_metadata(link_type: LinkType<'_>) -> Result<Response<Page<TrackSimplified>>, Error> {
+    let credentials =
+        ClientCredentials::from_env().expect("CLIENT_ID and CLIENT_SECRET not found.");
     let client = Client::new(credentials);
 
     match link_type {
-        LinkType::SpotifyAlbum(uri) => {
-            client.albums().get_album_tracks(uri, None).await.unwrap()
-        }
-        _ => Err("Enum not implemented".into())
+        LinkType::SpotifyAlbum(uri) => Ok(client
+            .albums()
+            .get_album_tracks(uri, 20, 0, None)
+            .await
+            .unwrap()),
+        LinkType::SpotifyTrack(_) => todo!(),
+        LinkType::SpotifyPlaylist(_) => todo!(),
     }
 }
